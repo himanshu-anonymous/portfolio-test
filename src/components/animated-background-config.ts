b94d570dde137c04ce60d5b52d685bdaa6d6d33f
himanshu-anonymous <himanshupatilpianist@gmail.com>
2025-12-09 14:58:05 +0530

    fix: reduce keyboard scale on skills section

--- src/components/animated-background-config.ts
+++ src/components/animated-background-config.ts
@@ -35,7 +35,7 @@ export const STATES = {
   },
   skills: {
     desktop: {
-      scale: { x: 0.4, y: 0.4, z: 0.4 },
+      scale: { x: 0.25, y: 0.25, z: 0.25 },
       position: { x: 0, y: -40, z: 0 },
       rotation: {
         x: 0,
@@ -44,7 +44,7 @@ export const STATES = {
       },
     },
     mobile: {
-      scale: { x: 0.2, y: 0.2, z: 0.2 },
+      scale: { x: 0.15, y: 0.15, z: 0.15 },
       position: { x: 0, y: -40, z: 0 },
       rotation: {
         x: 0,
